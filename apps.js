80a8145fffc04f5dff7bdd65696f407c668aac92
koldfeet <chris.tr88@gmail.com>
2021-11-05 16:39:21 -0600

    html, css, js files added

---
+++ apps.js
@@ -0,0 +1,42 @@
+const today = new Date();
+console.log (today);
+const dayNumber = today.getDay();
+console.log (dayNumber);
+const element = document.getElementById("message1");
+
+function special () {
+    if (dayNumber === 1) {
+        return "1/2 off all oils";
+    }
+    else if (dayNumber === 2) {
+        return "2-for-1 treats";
+    }
+    else if (dayNumber === 3) {
+        return "Buy-one-Get-one Free day";
+    }
+    else if (dayNumber === 4) {
+        return "Add a gummy";
+    }
+    else if (dayNumber === 5) {
+        return "Bring a friend get a extra treat";
+    }
+    else if (dayNumber === 6) {
+        return "Buy two edibles get one free";
+    }
+    else if (dayNumber === 7) {
+        return "Brunch and free samples";
+    }
+    else (element.classList.add("hideme"))
+
+}
+
+console.log("Your got a " + special());
+element.innerHTML = "Check out the Special today" + "</br>" + special();
+
+//doggie years calculator
+// Your doggie is NN years old in dog years!
+var calcDogYear = function () {
+    var humanYear = document.getElementById("dog").value;
+    var realDogAge = humanYear * 7;
+    document.getElementById("dogAge").innerHTML = ("Your doggi is " + realDogAge + " years old in dog years!");
+}
