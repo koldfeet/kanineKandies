eeff51284e906ba71bd734f5138311e05a6d9784
koldfeet <chris.tr88@gmail.com>
2021-11-08 13:32:07 -0700

    denied page now work

---
+++ denied.js
@@ -0,0 +1,5 @@
+//denied page JS START
+var greeting = "Sorry! ";
+var message = "We cannot show you our content due to your age. "
+document.getElementById("deniedMessage").innerHTML = greeting + message;
+//denied page JS END
