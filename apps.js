82468c57eb041f4b687e9bd5b6e6a10c3e441d0b
koldfeet <chris.tr88@gmail.com>
2021-11-15 17:23:43 -0700

    dosage added to homepage

--- apps.js
+++ apps.js
@@ -51,14 +51,55 @@ const arrayCarousel = [
     "images/image2.png",
 ];
 
+// @ts-ignore
 counter = 0;
 
 function setImage () {
+    // @ts-ignore
     document.getElementById("carousel-image").src = arrayCarousel [counter];
     // document.getElementById("carousel-image1").src = arrayCarousel [counter];
 
+    // @ts-ignore
     counter = (counter + 1) % arrayCarousel.length;
 }
 
 setInterval (setImage , 2000);
  //Carousel END
+
+//dog CBD DOSAGE START
+const smallDog = {
+    oneQuarter: "1/4ml = 1.75mg",
+    half: "1/2ml = 3.5mg",
+    threeQuarter: "3/4ml = 5.25mg",
+    oneML: "1ml = 7mg",
+};
+
+const mediumDog = {
+    oneQuarter: "1/4ml = 4.25mg",
+    half: "1/2ml = 8.5mg",
+    threeQuarter: "3/4ml = 12.75mg",
+    oneML: "1ml = 17mg",
+};
+
+const largeDog = {
+    oneQuarter: "1/4ml = 6.75mg",
+    half: "1/2ml = 13.5mg",
+    threeQuarter: "3/4ml = 20.25mg",
+    oneML: "1ml = 27mg",
+};
+
+function dogDosage () {
+        var textOne = "Dogs: 5 - 35lbs";
+        var boldOne = textOne.bold();
+        var textTwo = "Dogs: 35 - 75lbs";
+        var boldTwo = textTwo.bold();
+        var textThree = "Dogs: 75lbs +";
+        var boldThree = textThree.bold();
+        document.getElementById("myDosage").innerHTML = boldOne  + "<br>" + smallDog.oneQuarter + "<br>" + smallDog.half + "<br>" +  smallDog.threeQuarter + "<br>" + smallDog.oneML ;
+        document.getElementById("myDosageTwo").innerHTML = boldTwo + "<br>" + mediumDog.oneQuarter + "<br>" + mediumDog.half + "<br>" +  mediumDog.threeQuarter + "<br>" + mediumDog.oneML ;
+        document.getElementById("myDosageThree").innerHTML = boldThree + "<br>" + largeDog.oneQuarter + "<br>" + largeDog.half + "<br>" +  largeDog.threeQuarter + "<br>" + largeDog.oneML ;
+};
+
+dogDosage ();
+
+//dog CBD DOSAGE END
