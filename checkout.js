d77cba080a2d13f64d6ecf963cb35e0bed25d103
koldfeet <chris.tr88@gmail.com>
2022-01-24 17:35:48 -0700

    everything works

--- checkout.js
+++ checkout.js
@@ -1,4 +1,4 @@
-//========make sure page load before JS load (if not it will not find these element)==========
+//========make sure page load before JS load (if not JS it will not find these element)==========
 if (document.readyState ==`loading`) {
     document.addEventListener(`DOMContentLoaded`, ready)
 } else {
@@ -10,19 +10,19 @@ function ready() {
     console.log(removeCartItemButtons) 
     for (var i = 0; i < removeCartItemButtons.length; i++) {
         var button = removeCartItemButtons[i]
-        button.addEventListener(`click`, removeCartItem)  //function on line 30
+        button.addEventListener(`click`, removeCartItem)  
     }
 
     var quantityInput = document.getElementsByClassName(`cart-quantity-input`) //change cart quantity input
     for (var i = 0; i < quantityInput.length; i++) {
         var input = quantityInput[i]
-        input.addEventListener(`change`, quantityChanged) //function on line 37
+        input.addEventListener(`change`, quantityChanged) 
     }
 
     var addToCartButtons = document.getElementsByClassName(`shop-item-button`) //add to cart botton
     for ( var i = 0; i < addToCartButtons.length; i++) {
         var button = addToCartButtons[i]
-        button.addEventListener(`click`, addToCartClicked) //function on line 46
+        button.addEventListener(`click`, addToCartClicked) 
     }
 
     document.getElementsByClassName(`btn-purchase`)[0].addEventListener(`click`, purchaseClicked)
@@ -32,10 +32,9 @@ function ready() {
 function purchaseClicked () {
     alert(`Thanks you for your purchases`)
     var cartItems = document.getElementsByClassName(`cart-items`)[0]
-    while (cartItems.hasChildNodes() ) { //this while loop will remove all cart items until empty
+    while (cartItems.hasChildNodes() ) { //this while loop will remove all cart items until empty when purchase is made
         cartItems.removeChild(cartItems.firstChild)
     }
-
     updateCartTotal() //this function update cart price after purchase
 }
 
@@ -43,43 +42,43 @@ function purchaseClicked () {
 function removeCartItem(event) {
     var buttonClicked = event.target
     buttonClicked.parentElement.parentElement.remove()
-    updateCartTotal()
+    updateCartTotal() //update price after removing item
 }
 
 //change quantity and price
 function quantityChanged (event) {
     var input = event.target
-    if (isNaN(input.value) || input.value <= 0 ) {
+    if (isNaN(input.value) || input.value <= 0 ) { //this If statement does not allow item quantity to go below 1
         input.value = 1
     }
-    updateCartTotal()
+    updateCartTotal() //update price after changing quantity
 }
 
-//add to cart button clicked
+//item title , price, image add to shopping cart when clicked
 function addToCartClicked(event) {
     var button = event.target
     var shopItem = button.parentElement.parentElement
-    var title = shopItem.getElementsByClassName(`shop-item-title`)[0].innerText
-    var price = shopItem.getElementsByClassName(`shop-item-price`)[0].innerText
-    var imageSrc = shopItem.getElementsByClassName(`shop-item-image`)[0].src
+    var title = shopItem.getElementsByClassName(`shop-item-title`)[0].innerText //adding item name when add to cart btn is click
+    var price = shopItem.getElementsByClassName(`shop-item-price`)[0].innerText //adding item price when add to cart btn is click
+    var imageSrc = shopItem.getElementsByClassName(`shop-item-image`)[0].src //adding item img when add to cart btn is click
     console.log(title , price , imageSrc) 
-    addItemToCart(title, price, imageSrc) //making a row for image,price,name (function on line 56)
+    addItemToCart(title, price, imageSrc) //making a row for image,price,name
     updateCartTotal()
 }  
 
 function addItemToCart (title, price, imageSrc) {  //this function is to add name,price,image, and also warn customer that item is alerady added to cart
-    var cartRow = document.createElement(`div`)
+    var cartRow = document.createElement(`div`)     
     cartRow.classList.add(`cart-row`)
     var cartItems = document.getElementsByClassName(`cart-items`)[0]
     var cartItmeNames = cartItems.getElementsByClassName(`cart-item-title`)
     for (var i = 0; i < cartItmeNames.length; i++) {
         // @ts-ignore
-        if (cartItmeNames[i].innerText == title ) {
+        if (cartItmeNames[i].innerText == title ) { //this for loop is to alert user's if items is already in shopping cart
             alert(`This item is already added to cart`)
             return
         }
     }
-    //adding all new items into car as a new row
+    //adding/appending all new items into cart as a new row
     var carRowContents = ` 
             <div class="cart-item cart-column">
                 <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
@@ -93,8 +92,8 @@ function addItemToCart (title, price, imageSrc) {  //this function is to add nam
     `
     cartRow.innerHTML = carRowContents
     cartItems.append(cartRow)
-    cartRow.getElementsByClassName(`btn-danger`)[0].addEventListener(`click`, removeCartItem) //making remove button work
-    cartRow.getElementsByClassName(`cart-quantity-input`)[0].addEventListener(`change`, quantityChanged) //change quantity price
+    cartRow.getElementsByClassName(`btn-danger`)[0].addEventListener(`click`, removeCartItem) //making remove button work by removing item when click
+    cartRow.getElementsByClassName(`cart-quantity-input`)[0].addEventListener(`change`, quantityChanged) //updating quantity price 
 }
 
 
@@ -111,7 +110,7 @@ for (var i = 0; i < removeCartItemButtons.length; i++) {
     })
 }
 
-//=========== function for price difference with button used for adding/removing============
+//=========== function for price difference when button used for adding/removing============
 function updateCartTotal() {
     var cartItemContainer = document.getElementsByClassName(`cart-items`)[0]
     var cartRows = cartItemContainer.getElementsByClassName(`cart-row`)
